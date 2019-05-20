68b339fe125f4d852e0c5ca9214cae70ab80b274
Matthew Reese <awesomepop012@hotmail.com>
2019-05-20 16:43:40 -0700

    Updated player movement and added vscode settings.
    A much better form of jumping was added that involves multiplying gravity as the player falls. It also allows you to hold the spacebar to gain more height.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,19 @@ public class PlayerMovement : MonoBehaviour
 
 	[Header("Movement:")]
 	[SerializeField]
-	private float m_moveForce = 365f;
+	private float m_moveVelocity = 4f;
 	[SerializeField]
-	private float m_maxSpeed = 5f;
+	private float m_maxSpeed = 8f;
 
 	[Header("Jumping:")]
 	[SerializeField]
-	private float m_jumpForce = 450f;
+	private float m_jumpVelocity = 17f;
+	[SerializeField]
+	[Tooltip("The amount that gravity will be multiplied by to increase fall speed.")]
+	private float m_fallMultiplier = 10f;
+	[SerializeField]
+	[Tooltip("The amount that gravity will be multiplied by when the player JUST taps the jump button.")]
+	private float m_lowJumpMultiplier = 8f;
 	[SerializeField]
 	[Tooltip("The time that the player has to recover after falling off a platform. (Milliseconds)")]
 	[Range(50, 1000)]
@@ -38,15 +44,16 @@ public class PlayerMovement : MonoBehaviour
 	private bool m_facingRight = true;
 	private bool m_jumping = false;
 	private bool m_canJump = false;
+	private bool m_jumpButton = false;
 	private bool m_grounded = false;
 
 	// Use this for initialization
-	void Start()
+	void Awake()
 	{
 		m_rb = GetComponent<Rigidbody2D>();
 	}
 
-
+	// Always do input in the Update method
 	void Update()
 	{
 		// Bitshift 1 the value of the LayerMask
@@ -63,6 +70,9 @@ public class PlayerMovement : MonoBehaviour
 		m_horizontal = Input.GetAxis("Horizontal");
 
 		m_vertical = Input.GetAxis("Vertical");
+
+		m_jumpButton = Input.GetButton("Jump");
+
 		if (m_canJump)
 		{
 			if (Input.GetButtonDown("Jump") && m_grounded)
@@ -75,18 +85,33 @@ public class PlayerMovement : MonoBehaviour
 	void FixedUpdate()
 	{
 		if (m_horizontal * GetComponent<Rigidbody2D>().velocity.x < m_maxSpeed)
-			m_rb.AddForce(m_horizontal * Vector2.right * m_moveForce);
+			m_rb.velocity = new Vector2(m_horizontal * m_moveVelocity, m_rb.velocity.y);
 
 		if (Mathf.Abs(m_rb.velocity.x) > m_maxSpeed)
 			m_rb.velocity = new Vector2(Mathf.Sign(m_rb.velocity.x) * m_maxSpeed, m_rb.velocity.y);
 
+		/********************JUMPING********************/
 		if(m_jumping)
 		{
-			m_rb.AddForce(new Vector2(0f, m_jumpForce));
+			m_rb.velocity = new Vector2(m_rb.velocity.x, m_jumpVelocity);
 			m_jumping = false;
 			m_canJump = false;
 		}
+		// If the player is falling
+		if (m_rb.velocity.y < 0)
+		{
+			// Then multiply the amount of gravity applied to the player, to fall faster
+			m_rb.velocity += Vector2.up * Physics2D.gravity.y * (m_fallMultiplier - 1) * Time.deltaTime;
+		}
+		// If the player is going up and the jump button isn't pressed
+		else if (m_rb.velocity.y > 0 && !m_jumpButton)
+		{
+			// Then make the player fall faster with low jump multiplier
+			m_rb.velocity += Vector2.up * Physics2D.gravity.y * (m_lowJumpMultiplier - 1) * Time.deltaTime;
+		}
+		/********************JUMPING********************/
 
+		/********************HORIZONTAL MOVEMENT********************/
 		if (m_horizontal > 0 && !m_facingRight)
 		{
 			m_facingRight = true;
@@ -97,5 +122,6 @@ public class PlayerMovement : MonoBehaviour
 			m_facingRight = false;
 			gameObject.transform.localScale = new Vector3(-1, 1, 1);
 		}
+		/********************HORIZONTAL MOVEMENT********************/
 	}
 }
